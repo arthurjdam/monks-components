db716b0b972c65a00fb7edf13a3bf83da94e6b46
arthurjdam <arthurjdam@gmail.com>
2021-04-22 15:11:55 -0700

    Add nextjs example (wip)

---
+++ examples/nextjs/pages/_app.tsx
@@ -0,0 +1,5 @@
+function MyApp({ Component, pageProps }): React.ReactElement {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ examples/nextjs/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import { Button } from '@arthurjdam/monks-components-core';
+
+export default function Home(): React.ReactElement {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <Button>asdf</Button>
+    </div>
+  );
+}
